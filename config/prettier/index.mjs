8e0421e9b92247b0babd14910f1e423f86181415
DaniloSVJ <danilocotatoceu@gmail.com>
2025-12-27 23:48:41 -0300

    criando pacote de autenticacao

---
+++ config/prettier/index.mjs
@@ -0,0 +1,17 @@
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'eas',
+    bracketSpacing: true,
+    arrowParens:    'always',
+    endOfLine: 'auto',
+    bracketSameLine: false
+}
+
+export default config
